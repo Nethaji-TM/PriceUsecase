9de239f2c086cc64bb08c97a24928444c2211269
Nethaji-TM <nethaji.tm@gmail.com>
2025-02-04 19:18:49 +0530

    Added swagger implementation

--- src/main/java/com/usecase/controller/PricingController.java
+++ src/main/java/com/usecase/controller/PricingController.java
@@ -6,26 +6,27 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.usecase.handler.PriceNotFoundException;
 import com.usecase.model.Price;
 import com.usecase.service.PriceService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
-@RequestMapping("/pricing/v1")
+@RequestMapping("/api/pricing/v1")
 public class PricingController {
 	@Autowired
 	private PriceService priceService;
 	private static final Logger LOGGER = LoggerFactory.getLogger(PricingController.class);
 
 	@GetMapping("/prices/{storeID}/{articleID}")
+	@Operation(summary = "Fetch prices by store Id", description = "Fetch the prices of the product by store Id")
 	public Map<String, Object> getPrices(@PathVariable String storeID, @PathVariable String articleID,
 			@RequestParam int page, @RequestParam int pageSize) {
 		
@@ -39,6 +40,7 @@ public class PricingController {
 	}
 	
 	@GetMapping("/getAllPrices")
+	@Operation(summary = "Fetch all prices", description = "Fetch all prices")
     public List<Price> getPrices() {
 		LOGGER.info("Fetching all prices");
         return priceService.findAll();
